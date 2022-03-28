79d7d7cf949a9504b1e9dff079be5e8d9c9e7034
Emmanuel Hombrebueno <emmanuel.hombrebueno@edu.sait.ca>
2022-03-27 19:36:55 -0600

    lab7

---
+++ src/main/java/ca/sait/lab7/dataaccess/RoleDB.java
@@ -0,0 +1,24 @@
+package ca.sait.lab7.dataaccess;
+
+import ca.sait.lab7.models.Role;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+public class RoleDB {
+
+    public List<Role> getAll() throws Exception {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        
+        try {
+            Query query = em.createNamedQuery("Role.findAll");
+            return query.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+}
